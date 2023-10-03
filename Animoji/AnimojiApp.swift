15527721af315bab43d77d7e6a2364e04b8d77e1
Syed Mansoor <imaco.mansoor@gmail.com>
2023-10-04 01:20:47 +0530

    Initial Commit

---
+++ Animoji/AnimojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimojiApp.swift
+//  Animoji
+//
+//  Created by Syed Mansoor on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animoji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animoji
+//
+//  Created by Syed Mansoor on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
